3f909ad1a4ea0e80195a009169f964616f85100a
Estudiante <antoniocalzado02@gmail.com>
2022-10-03 09:59:06 +0200

    Clase java, Clase DAO

--- src/main/java/com/jacaranda/dao/Dao.java
+++ src/main/java/com/jacaranda/dao/Dao.java
@@ -5,7 +5,12 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+
+import com.jacaranda.shoes.Shoes;
 
 public class Dao {
 	
@@ -13,6 +18,7 @@ public class Dao {
 	private static final String URL = "jdbc:mysql://localhost:3306/server?useSSL=false";
 	private static final String USUARIO = "dummy";
 	private static final String CLAVE = "dummy";
+	
 	
 	 Scanner myObj = new Scanner(System.in);
 
@@ -36,25 +42,113 @@ public class Dao {
 		return conexion;
 	}
 	
+	public void addShoes(int idShoes,String name,double price,int sizes, int year, int month,int day, boolean stock){
+		Connection conexion=this.conectar();
+		try {
+			Statement instruccion=conexion.createStatement();
+			String query = "INSERT INTO ZAPATILLA (ID, NAME, PRICE, SIZES, RELEASEDATE, STOCK) VALUES ("+ idShoes+", "+"'"+ name+"'"+", "+price+", "+sizes+", "+"'"+year+"-"+month+"-"+day+"'"+", "+stock+")";
+			instruccion.executeUpdate(query);
+		} catch (SQLException e) {
+			System.out.println("Eroor");
+			e.printStackTrace();
+		}
+			
+		
+	}
+	
+	public void deleteShoes(int idShoes) {
+		Connection conexion=this.conectar();
+		
+		try {
+			Statement instruccion=conexion.createStatement();
+			String query = "DELETE FROM ZAPATILLA WHERE ID="+idShoes;
+			instruccion.executeUpdate(query);
+		} catch (SQLException e) {
+			System.out.println("Eroor");
+			e.printStackTrace();
+		}
+	}
+	
+	public void updateShoes(int idShoes,String name,double price,int sizes, boolean stock){
+		Connection conexion=this.conectar();
+		if (name.isEmpty()) {
+			HashSet<Shoes> lista = this.loadList();
+			for(Shoes e:lista) {
+				if (e.getIdShoes()==idShoes){
+					name=e.getName();
+				}
+			}
+		}
+		if (price<=0) {
+			HashSet<Shoes> lista = this.loadList();
+			for(Shoes e:lista) {
+				if (e.getIdShoes()==idShoes){
+					price=e.getPrice();
+				}
+			}
+		}
+		if (sizes<=35 || sizes>=52) {
+			HashSet<Shoes> lista = this.loadList();
+			for(Shoes e:lista) {
+				if (e.getIdShoes()==idShoes){
+					sizes=e.getSizes();
+				}
+			}
+		}
+		if (stock!=true || stock!=false) {
+			HashSet<Shoes> lista = this.loadList();
+			for(Shoes e:lista) {
+				if (e.getIdShoes()==idShoes){
+					stock=e.isStock();
+				}
+			}
+		}
+		try {
+			Statement instruccion=conexion.createStatement();
+			String query = "UPDATE ZAPATILLA SET NAME="+"'"+name+"'"+", PRICE="+price+", SIZES="+sizes+", STOCK="+stock+" WHERE ID="+idShoes;
+			System.out.println(query);
+			instruccion.executeUpdate(query);
+			
+		} catch (SQLException e) {
+			System.out.println("Eroor");
+			e.printStackTrace();
+		}
+			
+		
+	}
 	
 	
-	public static void main(String[] args) {
-		Dao d=new Dao();
-		Connection conexion=d.conectar();
+	
+	public HashSet<Shoes> loadList() {
+		Connection conexion=this.conectar();
+		HashSet<Shoes> shoesList= new HashSet<>();
 		try {
 			Statement instruccion=conexion.createStatement();
 			String query = "SELECT * FROM ZAPATILLA";
 			ResultSet resultado=instruccion.executeQuery(query);
 			while (resultado.next()) {
 		
-				System.out.println("Nombre del autor: " + resultado.getString("NAME"));
-				System.out.println("******************************************");
-
+				Shoes shoesLoad=new Shoes(resultado.getInt("ID"), resultado.getString("NAME"), resultado.getDouble("PRICE"), resultado.getInt("SIZES"), resultado.getDate("RELEASEDATE"), resultado.getBoolean("STOCK"));
+				shoesList.add(shoesLoad);
 			}
 		} catch (SQLException e) {
-			System.out.println("Eroor");
+			System.out.println("Error");
 			e.printStackTrace();
 		}
+		return shoesList;
+	}
+	
+	
+	public static void main(String[] args) {
+		Dao d=new Dao();
+		Connection conexion=d.conectar();
+		d.updateShoes(121, "juanki", 0, 0, false);
+		HashSet<Shoes> lista = d.loadList();
+		for(Shoes e:lista) {
+			System.out.println(e.toString());
+		}
+		
+		
 		
 
 	}
